d7ecf83b3eeff19cdbd4e6dd4972b4bdb3ad89af
Mark Wong <markwong@Marks-MBP-16.attlocal.net>
2021-03-04 16:20:14 -0800

    Committer: Mark Wong <markwong@Marks-MBP-16.attlocal.net>
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   public/index.html
    	modified:   src/App.js
    
     Untracked files:
    	public/egg.jpg
    	public/plant.jpg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const names = ['James', 'Paul', 'John', 'George', 'Ringo'];
+ 
+  return ( 
+  <div>
+      <h1>headings are fun</h1>
+      <Folder name="Desktop"> 
+        <Folder name="logi">
+          <File name="file2" />
+        </ Folder> 
+        <File name="file" />
+      </Folder>
+      <Folder name="temp"></Folder>
+  </div>
   );
 }
+
+
+
+const Folder = (props) => {
+  const [ isOpen, setIsOpen ] = useState(false);
+  const { name, children } = props;
+
+  const handleClick = () => {setIsOpen(!isOpen);}
+  const handleEnter = (e) => {
+    console.log('hovered');
+    e.target.style.color = 'red';
+  }
+  const handleExit = (e) => {
+    e.target.style.color = 'black';
+  }
+  const direction = isOpen ? 'down' : 'right';
+
+  return (
+    <div style={{border: '1px solid pink'}}>
+      <span onClick={handleClick} onMouseEnter={handleEnter} onMouseLeave={handleExit}>
+      <i className ="folder icon"></i>
+      <i className ={`caret ${direction} icon`}></i>
+        {name}
+      </span>
+    {/* <div style = {{border: '1px solid blue', color: 'blue', marginLeft: '17px'}}> */}
+    <div>
+      { isOpen?props.children: null}
+     </div> 
+    </div>
+  );
+};
+
+
+
+
+const File = (props) => {
+  
+  return (
+    <div style = {{marginLeft: '40px'}} >
+      {props.name}
+    </div>
+  );
+};
 
 export default App;
